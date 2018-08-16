b35218a012e41b9d6a3404cf0c839f8f2f7e2d86
dimartiro <diego2737@gmail.com>
2018-08-16 13:51:20 -0300

    Fix url parsing and hash generation

--- services/short.go
+++ services/short.go
@@ -1,12 +1,21 @@
 package services
 
 import (
+	"regexp"
+
 	"github.com/dimartiro/urlshort/model"
 )
 
 const URL_SIZE = 7
 
 func Short(url string) string {
+
+	regex, _ := regexp.Compile("^(http|https)(://)")
+
+	if !regex.Match([]byte(url)) {
+		url = "http://" + url
+	}
+
 	hashedUrl := HashUrl(url, URL_SIZE)
 
 	model.SaveHashToUrl(hashedUrl, url)
